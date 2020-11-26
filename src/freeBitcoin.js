03790ef7e4766aac6e18067871c0f4dd10088950
Your Name <you@example.com>
2020-11-27 03:18:26 +0700

    update

--- src/freeBitcoin.js
+++ src/freeBitcoin.js
@@ -24,6 +24,7 @@ const freeBitcoin = {
     setTimeout(() => freeBitcoin.init(), 1000 * 60 * 60);
   },
   getHomePage: async () => {
+    console.log("Get bitcoin running ...");
     const browser = await puppeteer.launch();
 
     const page = await browser.newPage();
